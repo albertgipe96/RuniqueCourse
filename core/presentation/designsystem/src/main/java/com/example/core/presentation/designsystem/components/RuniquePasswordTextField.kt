75af85c420d27e5099811e3e855a4d4ced6320cb
albertgipe96 <albertgipe96@gmail.com>
2024-07-06 21:31:10 +0200

    RuniquePasswordTextField ui component

---
+++ core/presentation/designsystem/src/main/java/com/example/core/presentation/designsystem/components/RuniquePasswordTextField.kt
@@ -0,0 +1,134 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
+package com.example.core.presentation.designsystem.components
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text2.BasicSecureTextField
+import androidx.compose.foundation.text2.BasicTextField2
+import androidx.compose.foundation.text2.TextFieldDecorator
+import androidx.compose.foundation.text2.input.TextFieldLineLimits
+import androidx.compose.foundation.text2.input.TextFieldState
+import androidx.compose.foundation.text2.input.TextObfuscationMode
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.core.presentation.designsystem.EyeClosedIcon
+import com.example.core.presentation.designsystem.EyeOpenedIcon
+import com.example.core.presentation.designsystem.LockIcon
+import com.example.core.presentation.designsystem.R
+
+@Composable
+fun RuniquePasswordTextField(
+    modifier: Modifier = Modifier,
+    state: TextFieldState,
+    isPasswordVisible: Boolean,
+    onTogglePasswordVisibility: () -> Unit,
+    hint: String = "",
+    title: String? = null,
+) {
+    var isFocused by remember { mutableStateOf(false) }
+    Column(
+        modifier = modifier
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            title?.let {
+                Text(
+                    text = it,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(4.dp))
+        BasicSecureTextField(
+            modifier = Modifier
+                .clip(RoundedCornerShape(16.dp))
+                .background(
+                    if (isFocused) MaterialTheme.colorScheme.primary.copy(alpha = 0.05F)
+                    else MaterialTheme.colorScheme.surface
+                )
+                .border(
+                    width = 1.dp,
+                    color = if (isFocused) MaterialTheme.colorScheme.primary
+                    else Color.Transparent,
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .padding(horizontal = 12.dp)
+                .onFocusChanged {
+                    isFocused = it.isFocused
+                },
+            state = state,
+            textObfuscationMode = if (isPasswordVisible) TextObfuscationMode.Visible
+                                  else TextObfuscationMode.Hidden,
+            keyboardType = KeyboardType.Password,
+            textStyle = LocalTextStyle.current.copy(
+                color = MaterialTheme.colorScheme.onBackground
+            ),
+            cursorBrush = SolidColor(MaterialTheme.colorScheme.onBackground),
+            decorator = TextFieldDecorator { innerBox ->
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = LockIcon,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Box(modifier = Modifier.weight(1F)) {
+                        if (state.text.isEmpty() && !isFocused) {
+                            Text(
+                                modifier = Modifier.fillMaxWidth(),
+                                text = hint,
+                                color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.4F)
+                            )
+                        }
+                        innerBox()
+                    }
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Icon(
+                        imageVector = if (isPasswordVisible) EyeOpenedIcon else EyeClosedIcon,
+                        contentDescription = if (isPasswordVisible) stringResource(id = R.string.hide_password)
+                                             else stringResource(id = R.string.show_password),
+                        tint = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                }
+            }
+        )
+    }
+}

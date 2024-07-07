49916d0f20a9707fa9218ef4bb5d894bb2a118a6
albertgipe96 <albertgipe96@gmail.com>
2024-07-07 20:00:25 +0200

    Fix RegisterScreen color

--- auth/presentation/src/main/java/com/example/auth/presentation/register/RegisterScreen.kt
+++ auth/presentation/src/main/java/com/example/auth/presentation/register/RegisterScreen.kt
@@ -95,7 +95,7 @@ fun RegisterScreen(
                 withStyle(
                     style = SpanStyle(
                         fontFamily = Poppins,
-                        color = RuniqueGray
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
                     )
                 ) {
                     append(stringResource(id = R.string.already_have_an_account))

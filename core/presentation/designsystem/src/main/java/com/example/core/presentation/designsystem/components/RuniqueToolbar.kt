f8cc45afdbc6680668c1b76b20b6f9b15b826713
albertgipe96 <albertgipe96@gmail.com>
2024-07-08 19:47:42 +0200

    RuniqueToolbar ui component

---
+++ core/presentation/designsystem/src/main/java/com/example/core/presentation/designsystem/components/RuniqueToolbar.kt
@@ -0,0 +1,115 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.core.presentation.designsystem.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.core.presentation.designsystem.ArrowLeftIcon
+import com.example.core.presentation.designsystem.Poppins
+import com.example.core.presentation.designsystem.R
+import com.example.core.presentation.designsystem.components.util.DropdownItem
+
+@Composable
+fun RuniqueToolbar(
+    modifier: Modifier = Modifier,
+    title: String,
+    showBackButton: Boolean = false,
+    menuItems: List<DropdownItem> = emptyList(),
+    onMenuItemClick: (index: Int) -> Unit = {},
+    onBackClick: () -> Unit = {},
+    scrollBehavior: TopAppBarScrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(),
+    startContent: (@Composable () -> Unit)? = null
+) {
+    var isDropdownOpen by rememberSaveable { mutableStateOf(false) }
+    TopAppBar(
+        modifier = modifier,
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                startContent?.invoke()
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = title,
+                    fontWeight = FontWeight.SemiBold,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontFamily = Poppins
+                )
+            }
+        },
+        scrollBehavior = scrollBehavior,
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.Transparent
+        ),
+        navigationIcon = {
+            if (showBackButton) {
+                IconButton(onClick = onBackClick) {
+                    Icon(
+                        imageVector = ArrowLeftIcon,
+                        contentDescription = stringResource(id = R.string.go_back),
+                        tint = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+            }
+        },
+        actions = {
+            if (menuItems.isNotEmpty()) {
+                Box {
+                    DropdownMenu(
+                        expanded = isDropdownOpen,
+                        onDismissRequest = { isDropdownOpen = false }
+                    ) {
+                        menuItems.forEachIndexed { index, dropdownItem ->
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .clickable { onMenuItemClick(index) }
+                                    .padding(16.dp),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Icon(
+                                    imageVector = dropdownItem.icon,
+                                    contentDescription = dropdownItem.title
+                                )
+                                Spacer(modifier = Modifier.width(8.dp))
+                                Text(text = dropdownItem.title)
+                            }
+                        }
+                    }
+                    IconButton(onClick = { isDropdownOpen = true }) {
+                        Icon(
+                            imageVector = Icons.Default.MoreVert,
+                            contentDescription = stringResource(id = R.string.open_menu),
+                            tint = MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+                    }
+                }
+            }
+        }
+    )
+}

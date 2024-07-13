870934abd05c77d0c76b374076cb14933c752265
albertgipe96 <albertgipe96@gmail.com>
2024-07-13 16:31:20 +0200

    Room Local Database implementation

---
+++ core/database/src/main/java/com/example/core/database/mapper/RunMapper.kt
@@ -0,0 +1,37 @@
+package com.example.core.database.mapper
+
+import com.example.core.database.entity.RunEntity
+import com.example.core.domain.location.Location
+import com.example.core.domain.run.Run
+import org.bson.types.ObjectId
+import java.time.Instant
+import java.time.ZoneId
+import kotlin.time.Duration.Companion.milliseconds
+
+fun RunEntity.toRun(): Run {
+    return Run(
+        id = id,
+        duration = durationMillis.milliseconds,
+        dateTimeUtc = Instant.parse(dateTimeUtc).atZone(ZoneId.of("UTC")),
+        distanceMeters = distanceMeters,
+        location = Location(latitude, longitude),
+        maxSpeedKmh = maxSpeedKmh,
+        totalElevationMeters = totalElevationMeters,
+        mapPictureUrl = mapPictureUrl
+    )
+}
+
+fun Run.toRunEntity(): RunEntity {
+    return RunEntity(
+        id = id ?: ObjectId().toHexString(),
+        durationMillis = duration.inWholeMilliseconds,
+        distanceMeters = distanceMeters,
+        dateTimeUtc = dateTimeUtc.toInstant().toString(),
+        latitude = location.lat,
+        longitude = location.long,
+        avgSpeedKmh = avgSpeedKmh,
+        maxSpeedKmh = maxSpeedKmh,
+        totalElevationMeters = totalElevationMeters,
+        mapPictureUrl = mapPictureUrl
+    )
+}

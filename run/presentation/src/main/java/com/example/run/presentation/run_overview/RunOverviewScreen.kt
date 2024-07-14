50d7a07ed95d17832ac3e12452023d2826787e60
albertgipe96 <albertgipe96@gmail.com>
2024-07-14 13:17:22 +0200

    Displaying runs in RunOverviewScreen

--- run/presentation/src/main/java/com/example/run/presentation/run_overview/RunOverviewScreen.kt
+++ run/presentation/src/main/java/com/example/run/presentation/run_overview/RunOverviewScreen.kt
@@ -1,8 +1,14 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
+@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 
 package com.example.run.presentation.run_overview
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -10,6 +16,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.core.presentation.designsystem.AnalyticsIcon
@@ -21,6 +28,7 @@ import com.example.core.presentation.designsystem.components.RuniqueScaffold
 import com.example.core.presentation.designsystem.components.RuniqueToolbar
 import com.example.core.presentation.designsystem.components.util.DropdownItem
 import com.example.run.presentation.R
+import com.example.run.presentation.run_overview.components.RunListItem
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -29,11 +37,11 @@ fun RunOverviewScreenRoot(
     onStartRunClick: () -> Unit
 ) {
     RunOverviewScreen(
+        state = viewModel.state,
         onAction = { action ->
             when (action) {
-                RunOverviewAction.OnAnalyticsClick -> Unit
-                RunOverviewAction.OnLogoutClick -> Unit
                 RunOverviewAction.OnStartClick -> onStartRunClick()
+                else -> Unit
             }
             viewModel.onAction(action)
         }
@@ -42,6 +50,7 @@ fun RunOverviewScreenRoot(
 
 @Composable
 fun RunOverviewScreen(
+    state: RunOverviewState,
     onAction: (RunOverviewAction) -> Unit
 ) {
     val topAppBarState = rememberTopAppBarState()
@@ -79,6 +88,24 @@ fun RunOverviewScreen(
             )
         }
     ) { paddingValues ->
-        
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .nestedScroll(scrollBehavior.nestedScrollConnection)
+                .padding(horizontal = 16.dp),
+            contentPadding = paddingValues,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            items(
+                items = state.runs,
+                key = { it.id }
+            ) {
+                RunListItem(
+                    modifier = Modifier.animateItemPlacement(),
+                    runUi = it,
+                    onDeleteClick = { onAction(RunOverviewAction.DeleteRun(it)) }
+                )
+            }
+        }
     }
 }
